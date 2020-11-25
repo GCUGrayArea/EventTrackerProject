68c5d2ed7d316eff57bf10c9a628b6fdcba0c563
Gray Morrow <tgrahammor@gmail.com>
2020-11-24 18:09:37 -0700

    crossorigins added to controllers; Angular can work now

--- EventTrackerREST/src/main/java/com/skilldistillery/alccountant/controllers/BarTabController.java
+++ EventTrackerREST/src/main/java/com/skilldistillery/alccountant/controllers/BarTabController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.skilldistillery.alccountant.entities.BarTab;
 import com.skilldistillery.alccountant.services.BarTabService;
 
 @RequestMapping("api")
+@CrossOrigin({"*" , "localhost:4200"})
 @RestController
 public class BarTabController {
 	
